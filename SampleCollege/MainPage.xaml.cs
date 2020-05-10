f52c6be1d246fb675cb804cb8c56dbc8ad364703
K. Medha Chaitanya <medha_chaitanya@hotmail.com>
2020-05-10 16:37:57 -0400

    Implemented Screenshot functionality

--- SampleCollege/MainPage.xaml.cs
+++ SampleCollege/MainPage.xaml.cs
@@ -1,23 +1,26 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Composition;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Graphics;
+using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.UI;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,65 +32,79 @@ namespace SampleCollege
     public sealed partial class MainPage : Page
     {
         StorageFolder testFolder = null;
+        bool stopFlag = true;
+       
         public MainPage()
         {
             this.InitializeComponent();
+          
         }
 
+     
         private async void AppBarButton_Click(object sender, RoutedEventArgs e)
         {
-            FolderPicker picker = new FolderPicker() { SuggestedStartLocation = PickerLocationId.ComputerFolder };
-            picker.FileTypeFilter.Add(".png");
-            picker.FileTypeFilter.Add(".jpeg");
-            picker.FileTypeFilter.Add(".jpg");            
-            StorageFolder folder = await picker.PickSingleFolderAsync();
-            Windows.Storage.StorageFolder storageFolder = folder;
-            var files = await folder.GetFilesAsync();
-            await ApplicationData.Current.LocalFolder.CreateFolderAsync("ImagesFolder", CreationCollisionOption.ReplaceExisting);
-            
-            
-            foreach (var file in files)
+            try
             {
-                StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
 
-                string folderName = System.IO.Path.GetFileNameWithoutExtension(file.Name);  //folder name with filename
 
-                ////await ApplicationData.Current.LocalFolder.CreateFolderAsync("Data");//need to change the folder name with filename
+                FolderPicker picker = new FolderPicker() { SuggestedStartLocation = PickerLocationId.ComputerFolder };
+                picker.FileTypeFilter.Add(".png");
+                picker.FileTypeFilter.Add(".jpeg");
+                picker.FileTypeFilter.Add(".jpg");
+                StorageFolder folder = await picker.PickSingleFolderAsync();
+                Windows.Storage.StorageFolder storageFolder = folder;
+                var files = await folder.GetFilesAsync();
+                await ApplicationData.Current.LocalFolder.CreateFolderAsync("ImagesFolder", CreationCollisionOption.ReplaceExisting);
 
-                testFolder = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFolderAsync("ImagesFolder", CreationCollisionOption.OpenIfExists);
 
-                ////StorageFolder newFolder = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
-
-                //StorageFolder newFolder = await testFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
-
-                //string desiredName = file.Name;
-                ////should copy it to subfolder and raise alert if already exist
-
-                ////StorageFile newFile = await localFolder.CreateFileAsync(desiredName, CreationCollisionOption.FailIfExists);
-
-                try
+                foreach (var file in files)
                 {
-                    await file.CopyAsync(testFolder, file.Name, NameCollisionOption.FailIfExists);
-                }
-                catch (Exception exp)
-                {
-                    //show here messagebox that is exists
-                    Windows.UI.Xaml.Controls.ContentDialog replacePromptDialog = new Windows.UI.Xaml.Controls.ContentDialog()
+                    StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+                    string folderName = System.IO.Path.GetFileNameWithoutExtension(file.Name);  //folder name with filename
+
+                    ////await ApplicationData.Current.LocalFolder.CreateFolderAsync("Data");//need to change the folder name with filename
+
+                    testFolder = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFolderAsync("ImagesFolder", CreationCollisionOption.OpenIfExists);
+
+                    ////StorageFolder newFolder = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+
+                    //StorageFolder newFolder = await testFolder.CreateFolderAsync(folderName, CreationCollisionOption.OpenIfExists);
+
+                    //string desiredName = file.Name;
+                    ////should copy it to subfolder and raise alert if already exist
+
+                    ////StorageFile newFile = await localFolder.CreateFileAsync(desiredName, CreationCollisionOption.FailIfExists);
+
+                    try
                     {
-                        Title = "File exists in the new location",
-                        Content = "Do you want to replace the old file with the new file?",
-                        CloseButtonText = "Keep the old one",
-                        PrimaryButtonText = "Replace with new one"
-                    };
-                    Windows.UI.Xaml.Controls.ContentDialogResult result = await replacePromptDialog.ShowAsync();
-                    if (result == Windows.UI.Xaml.Controls.ContentDialogResult.Primary)
+                        await file.CopyAsync(testFolder, file.Name, NameCollisionOption.FailIfExists);
+                    }
+                    catch (Exception exp)
                     {
-                       // await file.CopyAsync(newFolder, desiredName, NameCollisionOption.ReplaceExisting);
+                        //show here messagebox that is exists
+                        Windows.UI.Xaml.Controls.ContentDialog replacePromptDialog = new Windows.UI.Xaml.Controls.ContentDialog()
+                        {
+                            Title = "File exists in the new location",
+                            Content = "Do you want to replace the old file with the new file?",
+                            CloseButtonText = "Keep the old one",
+                            PrimaryButtonText = "Replace with new one"
+                        };
+                        Windows.UI.Xaml.Controls.ContentDialogResult result = await replacePromptDialog.ShowAsync();
+                        if (result == Windows.UI.Xaml.Controls.ContentDialogResult.Primary)
+                        {
+                            // await file.CopyAsync(newFolder, desiredName, NameCollisionOption.ReplaceExisting);
+                        }
                     }
                 }
-            }            
-            Process(testFolder);
-            refreshButton.IsEnabled = true;
+                Process(testFolder);
+                refreshButton.IsEnabled = true;
+                stopFlag = false;
+            }
+            catch(Exception exx)
+            {
+                Debug.WriteLine("Exception");
+            }
         }
 
         private async void Process(StorageFolder storageFolder)
@@ -103,7 +120,7 @@ namespace SampleCollege
 
                     //add the image to the row sp
                     StorageFile currentImagefile = files2[fileIndex];
-                    var spV = new StackPanel() { Orientation = Orientation.Vertical, Margin = new Thickness(10) };
+                    var spV = new StackPanel() { Orientation = Orientation.Vertical, Margin = new Thickness(10), BorderBrush = new SolidColorBrush(Colors.Black), BorderThickness= new Thickness(imageBorderThickness.Value) };
                     BitmapImage bitmapImage = new BitmapImage();
                     Uri uri = new Uri(currentImagefile.Path, UriKind.RelativeOrAbsolute);
                     bitmapImage.UriSource = uri;
@@ -115,7 +132,7 @@ namespace SampleCollege
                         fileIndex += 1;
                     }
                     horiSp.Children.Add(spV);
-                    if (fileIndex >= files2.Count - 1)
+                    if (fileIndex >= files2.Count)
                         break;
 
                 }
@@ -124,11 +141,43 @@ namespace SampleCollege
                     break;
             }
         }
-        private void AppBarButton_Click1(object sender, RoutedEventArgs e)
+        private async void AppBarButton_Click1(object sender, RoutedEventArgs e)
         {
-           
-            
-           // sp.Children.Add( new Image() { Source = bitmapImage , Stretch = Stretch.None , Width = 170, Height=170 });
+            // ScreenshotButton_ClickAsync(sender, e);
+            // Render to an image at the current system scale and retrieve pixel contents
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap();
+            await renderTargetBitmap.RenderAsync(grid);
+            var pixelBuffer = await renderTargetBitmap.GetPixelsAsync();
+
+            var savePicker = new FileSavePicker();
+            savePicker.DefaultFileExtension = ".png";
+            savePicker.FileTypeChoices.Add(".png", new List<string> { ".png" });
+            savePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            savePicker.SuggestedFileName = "snapshot.png";
+
+            // Prompt the user to select a file
+            var saveFile = await savePicker.PickSaveFileAsync();
+
+            // Verify the user selected a file
+            if (saveFile == null)
+                return;
+
+            // Encode the image to the selected file on disk
+            using (var fileStream = await saveFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, fileStream);
+
+                encoder.SetPixelData(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Ignore,
+                    (uint)renderTargetBitmap.PixelWidth,
+                    (uint)renderTargetBitmap.PixelHeight,
+                    DisplayInformation.GetForCurrentView().LogicalDpi,
+                    DisplayInformation.GetForCurrentView().LogicalDpi,
+                    pixelBuffer.ToArray());
+
+                await encoder.FlushAsync();
+            }
         }
 
         private void headerSize_TextChanged(object sender, TextChangedEventArgs e)
@@ -165,16 +214,37 @@ namespace SampleCollege
 
         private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
         {
-            ToggleSwitch swi = sender as ToggleSwitch;
-            if (swi.IsOn)
+            if (!stopFlag)
             {
-                subheaderTB.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                subheaderTB.Visibility = Visibility.Collapsed;
+                ToggleSwitch swi = sender as ToggleSwitch;
+                if (swi.IsOn)
+                {
+                    subheaderTB.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    subheaderTB.Visibility = Visibility.Collapsed;
+                }
             }
             
         }
+
+        private void ToggleSwitch_Toggled1(object sender, RoutedEventArgs e)
+        {
+            if (!stopFlag)
+            {
+                ToggleSwitch swi = sender as ToggleSwitch;
+                if (swi.IsOn)
+                {
+                    
+                }
+                else
+                {
+                    subheaderTB.Visibility = Visibility.Collapsed;
+                }
+            }
+
+        }
+       
     }
 }
